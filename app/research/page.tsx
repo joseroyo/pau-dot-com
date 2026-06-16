2ddbde8668442891171281bfe9fb416b70e33142
joseroyo <jose.royonoble@gmail.com>
2026-06-16 16:40:49 -0600

    Added CV and research pages, changed nav

---
+++ app/research/page.tsx
@@ -0,0 +1,14 @@
+import Window from "@/components/Window";
+
+export default function Research() {
+  return (
+    <Window title="Work in progress" className="w-[50%] self-center mt-20">
+      <h2>Hello!</h2>
+      <p>Welcome to my new webpage!!!</p>
+      <p>It's still a work in progress, but soon you'll be able to see:</p>
+      <ol>
+        <li><span>- My professional research!</span></li>
+      </ol>
+    </Window>
+  );
+}
