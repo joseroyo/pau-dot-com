7a518ddf201fd0a5631e91513b579bcd395e5de4
joseroyo <jose.royonoble@gmail.com>
2026-06-20 21:33:28 -0600

    Still need to iron out details, but page is viewable on mobile devices

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -52,44 +52,48 @@ export default function EventCard({ id, lifeEvent, date, rating, description, ph
   return (
     <article className="flex gap-4 relative">
       {photoUrl && (
-        <img src={photoUrl} alt={`${lifeEvent} photo`} className="h-[100%]" width={200} height={200} />
+        <img src={photoUrl} alt={`${lifeEvent} photo`} className="w-[auto] h-[100px] absolute sm:w-[200px] sm:h-[100%] sm:relative md:w-[auto] md:h-[100px] md:absolute lg:w-[150px] lg:h-[100%] lg:relative xl:w-[200px]" width={200} height={200} />
       )}
       <div className="w-[100%]">
         {onUpdate && (
           <button type="button" onClick={() => setIsEditing(true)} className="absolute right-0 text-primary hover:underline">Edit Desc.</button>
         )}
-        <h3 className="w-[75%]">{lifeEvent}</h3>
-        <p>Date: {date}</p>
-        {isEditing ? (
-          <div>
-            <StarRating value={editedRating} onChange={setEditedRating} />
-          </div>
-        ) : (
-          <StarDisplay value={rating} />
-        )}
-        {isEditing ? (
-          <div className="flex flex-col mt-1">
-            <textarea
-              value={editedText}
-              onChange={(e) => setEditedText(e.target.value)}
-              rows={5}
-              className="pr-2"
-            />
-            <article className="flex justify-between mt-3">
-              <button type="button" onClick={handleCancel} className="hover:underline">Cancel</button>
-              <button type="button" onClick={handleSave} className="hover:underline">Save</button>
-            </article>
-          </div>
-        ) : (
-          <p className={!isExpanded && isLongDesc ? "line-clamp-5" : ""}>
-            {description}
-          </p>
-        )}
-        {isLongDesc && !isEditing && (
-          <button type="button" onClick={() => setIsExpanded(!isExpanded)} className="text-pink-strong underline">
-            {isExpanded ? "Read less" : "Read more"}
-          </button>
-        )}
+        <div className="pl-[130px] sm:pl-0 md:pl-[130px] lg:pl-0">
+          <h3>{lifeEvent}</h3>
+          <p>Date: {date}</p>
+          {isEditing ? (
+            <div>
+              <StarRating value={editedRating} onChange={setEditedRating} />
+            </div>
+          ) : (
+            <StarDisplay value={rating} />
+          )}
+        </div>
+        <div className="mt-[35px] sm:mt-0 md:mt-[35px] lg:mt-0">
+          {isEditing ? (
+            <div className="flex flex-col mt-1">
+              <textarea
+                value={editedText}
+                onChange={(e) => setEditedText(e.target.value)}
+                rows={5}
+                className="pr-2"
+              />
+              <article className="flex justify-between mt-3">
+                <button type="button" onClick={handleCancel} className="hover:underline">Cancel</button>
+                <button type="button" onClick={handleSave} className="hover:underline">Save</button>
+              </article>
+            </div>
+          ) : (
+            <p className={!isExpanded && isLongDesc ? "line-clamp-3 sm:line-clamp-5" : ""}>
+              {description}
+            </p>
+          )}
+          {isLongDesc && !isEditing && (
+            <button type="button" onClick={() => setIsExpanded(!isExpanded)} className="text-pink-strong underline">
+              {isExpanded ? "Read less" : "Read more"}
+            </button>
+          )}
+        </div>
       </div>
       {onDelete && (
         <button type="button" className="absolute right-[-23px] top-[-59px] border-0 px-4 py-2 cursor-pointer" onClick={handleDelete}>x</button>
