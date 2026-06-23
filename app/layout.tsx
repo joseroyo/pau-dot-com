10f816fcbf47bc89860233f4e24ce0d04e29bd23
joseroyo <jose.royonoble@gmail.com>
2026-06-22 21:11:52 -0600

    restoring analytics for push

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
           <Navigation />
           {children}
         </AuthProvider>
-        {/*<Analytics />*/}
+        <Analytics />
       </body>
     </html>
   );
